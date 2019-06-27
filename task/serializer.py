11fecbfc373453782991b2311afcc26f346ac8cd
Noyon <mohaymenul.islam@mulytic.io>
2019-06-27 17:22:13 +0600

    create requirements.txt file

--- task/serializer.py
+++ task/serializer.py
@@ -3,6 +3,9 @@ from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Task
-        fields = '__all__'
+        # fields = '__all__'
+        read_only_fields = ('is_completed', 'is_deleted', 'create_at', 'update_at')
+        exclude = ('delete_at',)
